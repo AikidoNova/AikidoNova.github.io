b0e1c3242480310677ec7ee56d31bec85cb3132d
Victor Garcia <Victor.Garcia@ticketmaster.com>
2018-02-19 09:21:32 -0500

    Update ftp

--- scripts/uploadViaFTP.sh
+++ scripts/uploadViaFTP.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 cd ./_site
-find . -type f -exec curl -u $ANOVA_USER:$ANOVA_PASSWORD --ftp-create-dirs -T {} ftp://aikido-nova.org/htdocs/{} \;
+find . -type f -exec curl -u $ANOVA_USER:$ANOVA_PASSWORD --ftp-create-dirs -T {} ftp://home369638583.1and1-data.host/htdocs/{} \;
